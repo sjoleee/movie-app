50b13cd10657ed49d10fbfa1c7c959c51fcd8f8c
sjoleee <cent7425@gmail.com>
2022-07-06 00:35:53 +0900

    영화 api 호출

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const res = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=ff526875e06bdd32c68014b332f2ea4c"
+    );
+    const json = await res.json();
+    setMovies(json.results);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return <div>{loading ? <h1>hddi</h1> : null}</div>;
 }
 
 export default App;
